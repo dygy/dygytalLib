e4698bea7d3b705a4a390aa93f84d13dffe39e56
dygy <yukimurachris@gmail.com>
2019-05-01 16:25:21 +0300

    Sets methods

--- dygytal.js
+++ dygytal.js
@@ -1,15 +1,10 @@
 Object.defineProperty(Array.prototype, "allEqual", {
     get: function() {
         let firstValue = this[0];
-
         for(let value=1 ;value<this.length; value++) {
             if (this[value] !== firstValue) {
-                return false;
-            }
-        }
-
-        return true;
-    }
+                return false;}}
+        return true;}
 });
 Array.prototype.deleteIndex= function (index) {
     this.splice(index,1)
@@ -22,7 +17,30 @@ Array.prototype.deleteElementFromRight= function (value) {
     let index = this.lastIndexOf(value);
     this.splice(index,1)
 };
+Object.defineProperty(Set.prototype, "allEqual", {
+    get: function() {
+        let firstValue = this[0];
+        for(let value=1 ;value<this.size; value++) {
+            if (this.elem(value) !== firstValue) {
+                return false;}}
+        return true;}
+});
+Set.prototype.elem = function(index){
+    let iterator1 = this.values();
+    while (index > 1) {
+        iterator1.next().value;
+        index--;
+    }
+    return iterator1.next().value;
+};
 /*
+let set= new Set();
+set.add(2);
+set.add(2);
+console.log(set);
+console.log(set.elem(1));
+console.log(set.allEqual);
+
 let arr= [1,2,3,4,5,2];
 console.log(arr);
 arr.deleteIndex(0);
