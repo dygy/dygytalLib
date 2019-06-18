27eab021f8b61276732662d000c372b664c3e240
dygy <yukimurachris@gmail.com>
2019-06-18 23:44:56 +0300

    replace All + first try of json parser

--- pure/dygytal.js
+++ pure/dygytal.js
@@ -19,23 +19,17 @@ Object.defineProperty(Number.prototype, "reverse", {
         arr.reverse();
         if (arr.indexOf('.')!==-1){
             return parseFloat(arr.join(''))
-
         }
         else {
             return parseInt(arr.join(''))
-
-        }
-    }
-});
+        }}});
 Array.prototype.deleteAll= function (value) {
     while (this.includes(value)) {
         this.splice(this.indexOf(value),1)
-    }
-};
+    }};
 Array.prototype.deleteIndex= function (index) {
     this.splice(index,1)
 };
-
 Array.prototype.deleteElementFromLeft= function (value) {
     let index = this.indexOf(value);
     this.splice(index,1)
@@ -77,16 +71,45 @@ Array.prototype.insert = function(index) {
         Array.prototype.slice.call(arguments, 1)));
     return this;
 };
+String.prototype.replaceAll = function (value, withA) {
+    let regex = new RegExp(value,'g');
+    return this.replace(regex,withA)
+};
 
+/* To fix
+let jason = {
+    Cool:{
+        Age:0,
+        Sage:1
+    },
+    NotCool:{
+      Name:'',
+      Flame:''
+    }
+};
+console.log(findValue(jason ,'NotCool'));
+
+function findValue(json,fieldName) {
+    let str= JSON.stringify(json);
+    let regEx = new RegExp(fieldName+'\\:\\{(\\n|.)*?\\}');
+    if (regEx.test(str)) {
+        return regEx.exec(str)
+    }
+
+    console.log(regEx)
+    //TODO:: if '', if int, if array
+}
+//let str = 'hello'
+//write(str.replaceAll('l','2')) he22o
 //let int=125.39;
 //write(int.reverse);
 /*
 let set= new Set();
 set.add(1);
 set.add(2);
-console.log(set.elem(1)); //2
-console.log(set.allEqual);
-let arr= [1,2,3,4,5,2];
+write(set.elem(1)); //2
+write(set.allEqual);
+write [1,2,3,4,5,2];
 arr.deleteIndex(0); //2,3,4,5,2
 [1,1,2,1,3,1].deleteAll(1) //[2,3]
 */
